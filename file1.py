e37c34abddf13e1c022131185fcf4e8ab9d50e44
Sathvika <sathvikakanala002@gmail.com>
2024-12-25 16:37:58 +0530

    new

--- file1.py
+++ file1.py
@@ -1,1 +1,21 @@
-print("Hello")
+import csv 
+
+file_name="text.csv"
+
+#reading file
+def read_file(file_name):
+    file=open(file_name,"r")
+    csv_reader=csv.reader(file)
+    return csv_reader
+
+rows=read_file(file_name)
+
+#printing contents
+def print_contents(file_name):
+    for rows in rows:
+        print(rows)
+
+print_contents(file_name)
+
+
+
