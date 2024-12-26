1dbb7692cc56f3aee215cac10e42381fac4e16da
Sathvika <sathvikakanala002@gmail.com>
2024-12-26 16:42:44 +0530

    check now

--- file1.py
+++ file1.py
@@ -1,21 +1,50 @@
 import csv 
+import datetime
+import pandas as pd
 
-file_name="text.csv"
+file_name="test.csv"
 
 #reading file
 def read_file(file_name):
-    file=open(file_name,"r")
-    csv_reader=csv.reader(file)
-    return csv_reader
+    try:
+        df=pd.read_csv(file_name)
+        return df
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return None
+    
 
-rows=read_file(file_name)
+def validate_date(date):
+    try:
+        datetime.datetime.strptime(date, '%d/%m/%Y')
+    except ValueError:
+        return 'Invalid date format'
+    return None
 
 #printing contents
 def print_contents(file_name):
-    for rows in rows:
-        print(rows)
+    df=read_file(file_name )
+    if df is not None:
+        for _, row in df.iterrows():
+            print("\nRow Data:")
+            for col_name in df.columns:
+                print(f"{col_name}:{row[col_name]}")
+    print(tabulate(df, headers='keys', tablefmt='grid'))
 
+            
+    err= validate_date(row["Date"])
+    if err:
+        print("Error.")
+
+
+        
+
+#print(storeoutflowdata(file_name))
 print_contents(file_name)
+#print(read_file(file_name))
+    
+
+
 
 
 
