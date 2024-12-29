d8af5c1abb143be02794c9ebf1721e5b2b639f17
Sathvika <sathvikakanala002@gmail.com>
2024-12-29 20:14:40 +0530

    sum and average

--- file1.py
+++ file1.py
@@ -2,7 +2,7 @@ import csv
 import datetime
 import pandas as pd
 
-file_name="test.csv"
+file_name="aa.csv"
 
 #reading file
 def read_file(file_name):
@@ -41,13 +41,44 @@ def print_table(file_name):
     print(df.to_string(index=False, justify="center"))
         
 def amount_sum(amount):
+    total_sum=0
     df=read_file(file_name)
-
     
+    if amount in df.columns:
+        print(f"Elements under '{amount}':")
+        print(df[amount].to_string(index=False))
+        print("------------")
+        print("Number of amount enteries:",len(df))
+    else:
+        print(f"Column '{amount}' not found in the file.")
+
+def print_amount_sum(amount):
+    total_sum=0
+    df=read_file(file_name)
+    for row in range(len(df)):
+        total_sum+=row
+    print("-------------")
+    print("The total amount spent:", float(total_sum))
+    print("-------------")
+
+def average_amount(amount):
+    total_sum=0
+    df=read_file(file_name)
+    for row in range(len(df)):
+        total_sum+=row
+    average_amount=total_sum/(len(df))
+    print("Average amount spent:",float(average_amount))
+
+
+
+
 #print(storeoutflowdata(file_name))
 #print_contents(file_name)
 #print(read_file(file_name))
-print(print_table(file_name))
+#print(print_table(file_name))
+average_amount("Amount")
+print_amount_sum("Amount")
+
     
 
 
